e02d86c9546b063c79980d9685d1bda54e2b10c2
Mattk205 <mattk205@gmail.com>
2017-07-04 19:32:57 -0400

    Assignment 8 complete

--- scripts/albums.js
+++ scripts/albums.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumMatt = {
+    title: 'The Worst Album Ever',
+    artist: 'George Costanza',
+    label: 'EM',
+    year: '1989',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Yankees', duration: '1:01' },
+        { title: 'No Soup for You', duration: '5:01' },
+        { title: 'Susan', duration: '3:21'},
+        { title: 'Marble Rye', duration: '3:14' },
+        { title: 'Lobster Fishing', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -39,6 +54,11 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return template;
 };
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
     // #1
@@ -65,4 +85,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumMatt];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+      setCurrentAlbum(albums[index]);
+      index++;
+      if(index == albums.length){
+        index=0;
+      }
+    });
 };
